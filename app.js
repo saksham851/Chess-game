5c0be0e79af071a9c3eb75e4c6e88b842d1cfe13
Saksham <121371714+saksham851@users.noreply.github.com>
2024-06-11 12:00:41 +0530

    socket.io basics

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-const express=require("express");
+const express=require("express")
 const socket=require("socket.io")
+const http=require("http")
+const {Chess}=require ("chess.js")
+const path= require ('path')
+
 const app=express();
+const server=http.createServer(app);
+
+const io=socket(server);
+
+const chess=new Chess();
+const port=3000;
+let players={};
+let currentPlayer ="W";
+
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/',(req,res)=>{
+    res.render("index")
+})
+
+io.on("connection",function(uniquesocket){
+    console.log("connected");  
+    uniquesocket.on("disconnect",function(){
+        console.log("disconnect")
+    }) 
+})
+
+
+
+server.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+});
